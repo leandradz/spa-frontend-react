3368067aebe96c83680d3441a5e04f7a04d94438
Leticia de Andrade <leticia_andrade94@hotmail.com>
2022-07-03 17:50:23 -0300

    feat: create initial home page structure

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,1 +1,51 @@
-export const Home = () => <div>Home</div>;
+import { useState } from "react";
+import {
+  IconFavoriteOne,
+  IconHero,
+  IconToggleOff,
+} from "../../components/Icons";
+import { Logo } from "../../components/Logo";
+import {
+  CardList,
+  Centralize,
+  Container,
+  Header,
+  HeaderList,
+  Text,
+} from "./styles";
+
+export const Home = () => {
+  const [data, setData] = useState(undefined);
+
+  return (
+    <Container>
+      <Header>
+        <Logo />
+        <h1>EXPLORE O UNIVERSO</h1>
+        <Text isColorGray>
+          Mergulhe no domínio deslumbrante de todos os personagens clássicos que
+          você ama - e aqueles que você descobrirá em breve!
+        </Text>
+      </Header>
+
+      <input placeholder="Procurar por heróis" />
+      <HeaderList>
+        <Text>Encontrados X heróis</Text>
+
+        <div>
+          <IconHero />
+          <Text isColorRed>Ordenar por nome - A/Z</Text>
+
+          <IconToggleOff />
+        </div>
+
+        <div>
+          <IconFavoriteOne />
+          <Text isColorRed>Somente os favoritos</Text>
+        </div>
+      </HeaderList>
+
+      {!!data ? <CardList></CardList> : <Centralize>carregando...</Centralize>}
+    </Container>
+  );
+};
