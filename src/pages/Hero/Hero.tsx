e747efc5a6fdd219143325c2fa4a957209ab853c
Leticia de Andrade <leticia_andrade94@hotmail.com>
2022-07-03 18:37:40 -0300

    feat: create hero page initial structure

--- src/pages/Hero/Hero.tsx
+++ src/pages/Hero/Hero.tsx
@@ -1,1 +1,41 @@
-export const Hero = () => <div>Hero</div>;
+import { Link } from 'react-router-dom'
+import { LogoMinor } from '../../components/Logo'
+import { SearchBar } from '../../components/SearchBar/SearchBar'
+import {
+  Centralize,
+  Container,
+  Header,
+  Title,
+  ComicsSection,
+
+} from './styles'
+import { debounce } from 'lodash'
+import { useState } from 'react'
+
+export const Hero = () => {
+  const [searchValue, setSearchValue] = useState('')
+  const debouncedHandleSearch = debounce((value: string) => {
+    setSearchValue(value)
+  }, 1000)
+
+  return (
+    <Container>
+      <Header>
+        <Link to={`/`}>
+          <LogoMinor />
+        </Link>
+
+        <SearchBar setSearchValue={debouncedHandleSearch} />
+      </Header>
+      {!!character?.name ? (
+        <div>
+          <ComicsSection>
+            <Title>Últimos lançamentos</Title>
+          </ComicsSection>
+        </div>
+      ) : (
+        <Centralize>Personagem não encontrado</Centralize>
+      )}
+    </Container>
+  )
+}
